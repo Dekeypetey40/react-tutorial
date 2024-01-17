986ffc17c3e1b51ce48ac4dea3a1ecbf698d7f0e
Kai <kmichaelmikhail@gmail.com>
2024-01-17 14:30:35 +0100

    Add package-lock.json and package.json, update App.jsx

--- react-tutorial/src/App.jsx
+++ react-tutorial/src/App.jsx
@@ -1,2 +1,20 @@
 
-export default App
+import './App.css'
+
+export function App() {
+  return (
+    <>
+      <h1 className="center" style={{ color: 'red' }}></h1>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+createRoot(root).render(React.createElement("p", {}, "Hello World"));
+
+
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+const element = React.createElement("h1", {className:"title"}, "Online Supermarket");
+const root = document.querySelector("#react-root");
+createRoot(root).render(element);
