8fd6794c9757e40b68dab8b884c3b73a02fe76a7
Kai <kmichaelmikhail@gmail.com>
2024-02-05 15:04:53 +0100

    Add check for winner in handleClick function

--- react-tutorial/src/App.jsx
+++ react-tutorial/src/App.jsx
@@ -13,6 +13,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -42,4 +45,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], // horizontal
+    [3, 4, 5], // horizontal
+    [6, 7, 8], // horizontal
+    [0, 3, 6], // vertical
+    [1, 4, 7], // vertical
+    [2, 5, 8], // vertical
+    [0, 4, 8], // diagonal
+    [2, 4, 6], // diagonal
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
